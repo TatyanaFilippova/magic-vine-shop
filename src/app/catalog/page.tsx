0d1a95b594b8c2a2f2e798a213eb36a72c4fc6e6
dmitrachkovivan <dmitrachkovivan@yandex.ru>
2024-11-11 17:59:47 +0300

    feat: мерж

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,12 +1,13 @@
 import { Metadata } from "next";
 import CatalogPage from "./lib";
+import {Suspense} from "react";
 
 export const metadata: Metadata = {
   title: "Каталог изделий из бумажной лозы ручной работы",
 };
 
 const CatalogCardProduct = () => {
-  return <CatalogPage />;
+  return <Suspense fallback={'loading...'}><CatalogPage /></Suspense>;
 };
 
 export default CatalogCardProduct;
